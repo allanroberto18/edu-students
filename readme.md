daca3cc33a4c4c4e33f237667124e57c011e0100
allanroberto18 <allanroberto18@gmail.com>
2020-12-23 22:29:22 +0100

    Update configurations for database

---
+++ readme.md
@@ -0,0 +1,7 @@
+Get start
+
+On the terminal run and update the parameters considering your configurations:
+
+```
+cp .env.dist .env
+```
---
+++ src/main/resources/db/migration/V1__Create_Table_Student.sql
@@ -0,0 +1,7 @@
+CREATE TABLE student(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(125) NOT NULL,
+    `last_name` VARCHAR(125) NOT NULL,
+    `email` VARCHAR(125) NOT NULL,
+    PRIMARY KEY(`id`)
+)
